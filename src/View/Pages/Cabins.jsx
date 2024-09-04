f6ea77c887f6d26dc914380469628d1cf2dce53f
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2024-09-04 16:02:53 +0500

    supabase integrated

--- src/View/Pages/Cabins.jsx
+++ src/View/Pages/Cabins.jsx
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
+import { getCabins } from "../../Modal/Services/apiCabins";
 
 
 function Cabins() {
+useEffect(function(){
+  getCabins().then((data)=>console.log(data))
+},[])
+
   return (
   <div>
-    
+    <h2>Cabins</h2>
   </div>
   );
 }
