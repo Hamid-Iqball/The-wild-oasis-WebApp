aa8ddfb51e3191f3748a7a40f1cf956e9622ccb7
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2024-11-21 00:14:18 +0500

    completed the server side filtering

--- src/Modal/Services/apiBookings.js
+++ src/Modal/Services/apiBookings.js
@@ -2,13 +2,17 @@
 import supabase from "./supabase";
 
 // This function is to get all the bookings.
-export async function getBookings (){
+export async function getBookings (filter){
     
-let { data: bookings, error } = await supabase
+let query = supabase
 .from('bookings')
 // If we are selecting all the things then we will use (*), but if we want to use data from another table as well then we have to use that as well in with in the strings, we can get all data of the other table and also a single value related to the other table as well. e.g we are doing it in the below line.
 .select("id,created_at,startDate,endDate, numNights, numGuests,status,totalPrice , cabins(name) , guests(fullName,email)")
+// we can not use useSearchParams() hook here because this is just a normal function but we can use it in theuseBooking hook.
 
+// FILTER
+if(filter !== null) query =query[filter.method || 'eq'](filter.field , filter.value);
+let { data: bookings, error } = await query;
 if(error){
     console.error(error)
     throw new Error("Bookings could not loaded")
--- src/ViewModal/Hooks/BookingHooks/useBookings.js
+++ src/ViewModal/Hooks/BookingHooks/useBookings.js
@@ -1,12 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../../Modal/Services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+import { Field } from "@chakra-ui/react";
+import { RxValue } from "react-icons/rx";
 
 export function useBookings(){
+    // This is where we are using out client-side-filtering , the idea is that to pass the object that contains filter and sortby in data in the getBooking.
+    const [searchParams , setSearchParams] = useSearchParams()
+
+    const filterValue = searchParams.get("status");
+    const filter = !filterValue || filterValue==='all' ? null : {field:'status', value:filterValue , method:'eq'}
+
     const {data:bookings , isLoading , isError} = useQuery({
-        queryKey:['bookings'],
-        queryFn:getBookings
+        queryKey:['bookings', filter], //The queryKey is unqiue identifier used in React Query to manage and chache queries.it just like the dependecy array of the useQuery.This is one the 
+        queryFn:()=>getBookings(filter)
     })
 
-    console.log(bookings)
-    return {bookings, isLoading,isError}
+console.log(bookings)
+return {bookings, isLoading,isError}
 }
