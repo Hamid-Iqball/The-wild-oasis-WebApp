aa4b58b5c40331ea4df6711299cbb05ae22dbdd7
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2024-12-19 15:29:45 +0500

    added account updating feature and page in header

--- src/View/Pages/Account.jsx
+++ src/View/Pages/Account.jsx
@@ -2,7 +2,9 @@
 
 function Account() {
   return (
-   <div></div>
+    <div className="flex justify-between items-center mb-8 ">
+    <h2 className="text-orange-800 text-3xl font-[500]">Update your Account</h2>
+  </div>
   );
 }
 
--- src/View/UI/Header.jsx
+++ src/View/UI/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Logout from '../features/authentications/Logout';
+import { HiOutlineUser } from 'react-icons/hi2';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+const navigate = useNavigate()
+
   return (
     <div className="grid grid-cols-2 items-center h-16  px-4 ">
       <h1 className='text-xl'>HEADER</h1>
       <div className="justify-self-end">
+        <div className='flex justify-between items-center gap-2'>
+
+          <button className='p-1 hover:bg-slate-200 active:bg-slate-200 hover:rounded-md' onClick={()=>navigate("/account")}>
+          <HiOutlineUser size='24' color='#9a3412'/>
+          </button>
+       
+
         <Logout />
+        </div>
       </div>
     </div>
   );
