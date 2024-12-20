b3d3888d7c94bcbeeac32331061eb1036969f320
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2024-12-20 11:53:04 +0500

    completed the header design and redirection

---
+++ src/View/UI/Avatar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useUser } from '../../ViewModal/Hooks/AuthenticationsHooks/useUser'
+function Avatar() {
+
+const {user} = useUser()
+const {fullName , avatar} = user.user_metadata;
+
+  return (
+    <div className='flex justify-center items-center gap-1'>
+        <img src={avatar || 'default-user.jpg'} alt={`Avatar of ${fullName}`} className='w-10'/>
+        <p className='font-semibold text-orange-800 text-sm'>{fullName}</p>
+    </div>
+  )
+}
+
+export default Avatar
--- src/View/UI/Header.jsx
+++ src/View/UI/Header.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 import Logout from '../features/authentications/Logout';
 import { HiOutlineUser } from 'react-icons/hi2';
 import { useNavigate } from 'react-router-dom';
+import Avatar from './Avatar';
 
 function Header() {
 const navigate = useNavigate()
 
   return (
-    <div className="grid grid-cols-2 items-center h-16  px-4 ">
-      <h1 className='text-xl'>HEADER</h1>
+    <div className="grid grid-cols-1 items-center h-16  px-4 ">
       <div className="justify-self-end">
         <div className='flex justify-between items-center gap-2'>
-
+       <Avatar/>
           <button className='p-1 hover:bg-slate-200 active:bg-slate-200 hover:rounded-md' onClick={()=>navigate("/account")}>
           <HiOutlineUser size='24' color='#9a3412'/>
           </button>
-       
-
         <Logout />
         </div>
       </div>
