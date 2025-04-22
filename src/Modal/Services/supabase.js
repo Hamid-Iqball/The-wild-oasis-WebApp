c383bd2afd8f4de9dcb18a429f7f40a7ba3cb726
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-04-22 22:28:37 +0500

    changes code

--- src/Modal/Services/supabase.js
+++ src/Modal/Services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-export const supabaseUrl = 'https://vgsfnabpxjclsbxtblzd.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnc2ZuYWJweGpjbHNieHRibHpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5MzM4NDYsImV4cCI6MjA0MDUwOTg0Nn0.18QChAft0uTUwNFlQhmdKnQgotZvGOZslC8nHC9LLuk'
+export const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_API_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
+
